1e11bcb516b885fa8d567932670391b8392e21c0
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2023-10-12 21:19:56 -0600

    adding Screen as a friend class

--- include/Img.hpp
+++ include/Img.hpp
@@ -1,10 +1,13 @@
 #ifndef __IMG_HPP__
 #define __IMG_HPP__
 #include <fstream>
+#include "Screen.hpp"
 
 /// @brief  Simple img creator with @c char[] extracting it's own file format.
 class Img
 {
+    friend class Screen;
+
 private:
     int width, height;
     std::string file_name;
